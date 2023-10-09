cbfc3aa7d0a43d9e8f07e3206c6ba06fd6c565d1
MSI-90 <104754352+MSI-90@users.noreply.github.com>
2023-10-09 11:27:10 +0700

    ErrorController is edit

--- Weather/Controllers/ErrorController.cs
+++ Weather/Controllers/ErrorController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 using Weather.ViewModels;
 
 namespace Weather.Controllers
@@ -6,9 +7,10 @@ namespace Weather.Controllers
     public class ErrorController : Controller
     {
         [Route("Error/{statusCode}")]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error(int statusCode)
         {
-            var errorViewModel = new ErrorViewModel { StatusCode = statusCode };
+            var errorViewModel = new ErrorViewModel { StatusCode = statusCode, RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier };
             return View(errorViewModel);
         }
     }
--- Weather/Controllers/HomeController.cs
+++ Weather/Controllers/HomeController.cs
@@ -59,12 +59,6 @@ namespace Weather.Controllers
                 throw;
             }
         }
-
-        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        //public IActionResult Error()
-        //{
-        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        //}
     }
 
     //public IActionResult Privacy()
