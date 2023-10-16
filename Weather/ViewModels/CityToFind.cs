8211a8fb1fce70fc4734259cc6c9285a8278925a
MSI-90 <104754352+MSI-90@users.noreply.github.com>
2023-10-17 01:58:16 +0700

    CityToFind model has been modified just DataAnotation attribute

--- Weather/ViewModels/CityToFind.cs
+++ Weather/ViewModels/CityToFind.cs
@@ -4,7 +4,10 @@ namespace Weather.ViewModels
 {
     public class CityToFind
     {
-        [Required (ErrorMessage = "Необходимо указать наименование н.п."), MinLength(2), MaxLength(50), Display(Name = "Населенный пункт")]
+        [Required (ErrorMessage = "Необходимо указать наименование н.п.")]
+        [MinLength(2, ErrorMessage = "Минимальная длина 2 символа")]
+        [MaxLength(50, ErrorMessage = "Максимальная длина 50 символов")]
+        [Display(Name = "Населенный пункт")]
         public string City { get; set; } = string.Empty;
     }
 }
