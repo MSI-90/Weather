6376c772e041c8f4e8aed3754ec2b1e70ae4a8bc
MSI-90 <104754352+MSI-90@users.noreply.github.com>
2023-11-17 23:03:51 +0700

    CookieTools class has been edit

--- Weather/OtherTools/CookieTools.cs
+++ Weather/OtherTools/CookieTools.cs
@@ -4,8 +4,9 @@ using System.Net;
 namespace Weather.OtherTools
 {
     internal static class CookieTools
-    { 
-        internal static Queue<string> LastWeather {  get; set; } = new Queue<string>();
+    {
+        internal static List<string> LastWeather { get; private set; } = new List<string>();
+        internal static string[] elementsFromCookie { get; private set; }
         private const string _key = "city";
         private const int _numberOfCityes = 4;
         internal static void SetOnce(HttpContext? context, string value)
@@ -14,34 +15,47 @@ namespace Weather.OtherTools
             {
                 CookieOptions options = new()
                 {
+                    //SameSite = SameSiteMode.Strict,
                     HttpOnly = true,
                     Secure = true,
                     Expires = DateTimeOffset.UtcNow.AddMonths(1)
                 };
 
                 if (!context.Request.Cookies.ContainsKey(_key))
+                {
                     context.Response.Cookies.Append(_key, value, options);
+                    
+                }
                 else
                 {
                     if (context.Request.Cookies.TryGetValue(_key, out var cookie))
                     {
-                        string[] elementsFromCookie = cookie.Split("|");
-                        bool isIsset = elementsFromCookie.Contains(value);
+                        elementsFromCookie = cookie.Split("|");
 
-                        if (!isIsset && elementsFromCookie.Length < _numberOfCityes)
+                        if (!elementsFromCookie.Contains(value) && elementsFromCookie.Length < _numberOfCityes)
                         {
                             string newValue = cookie + "|" + value;
                             context.Response.Cookies.Append(_key, newValue, options);
-
-                            foreach (string city in elementsFromCookie)
-                            {
-                                LastWeather.Enqueue(city);
-                            }
                         }
-                    } 
+
+                        AddCity(LastWeather, elementsFromCookie, cookie);
+                    }
                 }
             }
         }
-
+        private static void AddCity(List<string> list, string[] array, string cookie)
+        {
+            array = !string.IsNullOrEmpty(cookie) ? cookie.Split("|") : new string[0];
+            if(array.Length > 0)
+            {
+                foreach (string value in array)
+                {
+                    if (!list.Contains(value))
+                    {
+                        list.Add(value);
+                    }
+                }
+            }
+        }
     }
 }
