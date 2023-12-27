6b84cfc0779f08afde40b5d40b00231d45c6b23a
MSI-90 <104754352+MSI-90@users.noreply.github.com>
2023-12-28 00:50:02 +0700

    1. site.css has been edit 2. site.js has been changed 3. details.cshtml has been edit

--- Weather/wwwroot/js/site.js
+++ Weather/wwwroot/js/site.js
@@ -104,6 +104,16 @@ $(document).ready(function () {
     $('.forecast').click(function () {
         Forecast();
     });
+
+
+    //const blocks = document.querySelectorAll('#firstDay, #secondDay, #threeDay');
+
+    //blocks.forEach(block => {
+    //    block.addEventListener('click', () => {
+    //        const data = block.querySelector('h5').textContent;
+    //    });
+    //});
+
 });
 
 function Forecast() {
@@ -111,7 +121,6 @@ function Forecast() {
         City: $('#сityName').val(),
         DayOfWeek: 3
     }
-
     $.ajax({
         url: "/forecast",
         data: JSON.stringify(obj),
@@ -119,7 +128,7 @@ function Forecast() {
         contentType: "application/json;charset=UTF-8",
         dataType: "json",
         success: function (result) {
-
+            
             let dayColor = [];
             for (let i = 0; i < 3; i++) {
                 if (result[i].dayOfWeek === "Суббота" || result[i].dayOfWeek === "Воскресенье") {
@@ -129,27 +138,33 @@ function Forecast() {
                     dayColor.push('blue');
                 }
             }
+            console.log(result);
 
             $('.three-days').css('display', 'block');
-            console.log(result);
-            $('#firstDay h5').text(result[0].date);
-            $('#firstDay h5').css('color', dayColor[0]);
+
+            $('.tab1 a').text(result[0].date);
+            $('.tab2 a').text(result[1].date);
+            $('.tab3 a').text(result[2].date);
+
+
+            /*$('#firstDay h5').text(result[0].date);*/
+            /*$('#firstDay h5').css('color', dayColor[0]);*/
             $('#firstDay .temp').text('Температура воздуха: ' + result[0].minTemp + ' °C' + ' ... ' + result[0].maxTemp + ' °C ');
             $('#firstDay .windGust').text('Ветер до: ' + result[0].windGust + ' м/с');
             $('#firstDay .tempIco').html('<img src= "' + result[0].weatherImg + '" alt = "' + result[0].weatherText + '"title = "' + result[0].weatherText + '"/>');
             $('#firstDay .visible').text('Горизонтальная видимость: ' + result[0].avgVisInKm + 'км');
             $('#firstDay .humidity').text('Влажность: ' + result[0].humidity);
 
-            $('#secondDay h5').text(result[1].date);
-            $('#secondDay h5').css('color', dayColor[1]);
+            //$('#secondDay h5').text(result[1].date);
+            $/*('#secondDay h5').css('color', dayColor[1]);*/
             $('#secondDay .temp').text('Температура воздуха: ' + result[1].minTemp + ' °C' + ' ... ' + result[1].maxTemp + ' °C ');
             $('#secondDay .windGust').text('Ветер до: ' + result[1].windGust + ' м/с');
             $('#secondDay .tempIco').html('<img src= "' + result[1].weatherImg + '" alt = "' + result[1].weatherText + '"title = "' + result[1].weatherText + '"/>');
             $('#secondDay .visible').text('Горизонтальная видимость: ' + result[1].avgVisInKm + 'км');
             $('#secondDay .humidity').text('Влажность: ' + result[1].humidity);
 
-            $('#threeDay h5').text(result[2].date);
-            $('#threeDay h5').css('color', dayColor[2]);
+            //$('#threeDay h5').text(result[2].date);
+            //$('#threeDay h5').css('color', dayColor[2]);
             $('#threeDay .temp').text('Температура воздуха: ' + result[2].minTemp + ' °C' + ' ... ' + result[2].maxTemp + ' °C ');
             $('#threeDay .windGust').text('Ветер до: ' + result[2].windGust + ' м/с');
             $('#threeDay .tempIco').html('<img src= "' + result[2].weatherImg + '" alt = "' + result[2].weatherText + '"title = "' + result[2].weatherText + '"/>');
@@ -157,4 +172,22 @@ function Forecast() {
             $('#threeDay .humidity').text('Влажность: ' + result[2].humidity);
         }
     });
-}
+};
+
+$(document).ready(function () {
+    $('.nav-tabs a').click(function (e) {
+        e.preventDefault(); // Предотвращает переход по ссылке
+
+        // Убираем класс 'active' у всех вкладок
+        $('.nav-tabs li').removeClass('active');
+        // Добавляем класс 'active' только к выбранной вкладке
+        $(this).parent('li').addClass('active');
+
+        // Получаем идентификатор выбранной вкладки
+        var tabId = $(this).attr('href');
+        // Скрываем все контенты вкладок
+        $('.tab-pane').removeClass('active');
+        // Отображаем только контент выбранной вкладки
+        $(tabId).addClass('active');
+    });
+});
